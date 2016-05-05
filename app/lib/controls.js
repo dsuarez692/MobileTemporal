6d40d389b61e39bc72a3b3885d9e1544ca2a2b54
unknown <daniel_suarez@edsatdltspi0180.EDSA-AR.com>
2016-05-05 17:29:51 -0300

    Primer Commit. Side Menu funcionando para android y windows phone

---
+++ app/lib/controls.js
@@ -0,0 +1,33 @@
+var Alloy=require('alloy');
+
+exports.getMainView=function(){
+	return Alloy.createController('mainview');;
+};
+
+exports.getMenuView=function(){
+	return Alloy.createController('menuview');	
+};
+
+exports.getMenuButton=function(args){
+	var v=Ti.UI.createView({
+		height: args.h,
+		width: args.w,
+		backgroundColor: '#E0E0E0'
+	});
+	
+	var b=Ti.UI.createView({
+		height: "30dp",
+		width: "30dp",
+		backgroundImage: "/media/image2.png",
+		
+	});
+	
+	v.add(b);
+	
+	return v;
+};
+
+//Get the Configuration Controller
+exports.getConfigView=function(){
+    return Alloy.createController('config');
+};
