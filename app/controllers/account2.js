6769aa4cbe1185ed3a10eea9992881701124b88d
Agustin Alvarez <agustinalvarez537@gmail.com>
2016-06-16 14:07:06 -0300

    Linea de negocio en mi cuenta

--- app/controllers/account2.js
+++ app/controllers/account2.js
@@ -3,6 +3,13 @@ var persistence = require('persistence');
 
 var user = persistence.getUserData();
 
+function ResetProductLine(){
+	$.lineFreshDairy.backgroundImage = "/media/danone-08.png";
+	$.lineEarlyLife.backgroundImage = "/media/danone-07.png";
+	$.lineWaters.backgroundImage = "/media/danone-06.png";
+	$.lineMedical.backgroundImage = "/media/danone-05.png";
+}
+
 exports.validateData = function (){
 	if($.bossname.value == ""){
 		alert('Debe ingresar el nombre de la persona a la que reporta');
@@ -27,10 +34,30 @@ exports.resetView = function(){
 };
 
 function loadDefaultValues(){
+	ResetProductLine();
 	if(user != null && user.name != undefined){
 		$.sector.value = user.sector;
 		$.bossname.value = user.bossname;
 		$.bosslast.value = user.bosslast;
+		$.comercial.value = user.Comercial;
+		switch(user.Comercial){
+			case "lineFreshDairy":{
+			$.lineFreshDairy.backgroundImage = "/media/danone-01.png";
+			break;	
+			}
+			case "lineEarlyLife":{
+				$.lineEarlyLife.backgroundImage = "/media/danone-02.png";
+				break;	
+			}
+			case "lineWaters":{
+				$.lineWaters.backgroundImage = "/media/danone-03.png";
+				break;	
+			}
+			case "lineMedical":{
+				$.lineMedical.backgroundImage = "/media/danone-04.png";
+				break;	
+			}
+		}
 	}
 }
 
